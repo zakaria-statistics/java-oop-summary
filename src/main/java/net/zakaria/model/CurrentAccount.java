7ff882686fca8efd210861f00f6c849e9fcfc39e
zakaria-statistics <65316874+zakaria-statistics@users.noreply.github.com>
2023-11-21 22:52:02 +0100

    fourth commit

--- src/main/java/net/zakaria/model/CurrentAccount.java
+++ src/main/java/net/zakaria/model/CurrentAccount.java
@@ -1,4 +1,30 @@
 package net.zakaria.model;
 
-public class SavingAccount extends BankAccount{
+public class CurrentAccount extends BankAccount{
+    private double overDraft;
+    public CurrentAccount(){
+        super();
+    }
+    public CurrentAccount(double balance, String currency, double overDraft){
+        super(balance, currency);
+        this.overDraft = overDraft;
+    }
+
+    public double getOverDraft() {
+        return overDraft;
+    }
+
+    public void setOverDraft(double overDraft) {
+        this.overDraft = overDraft;
+    }
+
+    @Override
+    public String toString() {
+        return "CurrentAccount { overDraft = "+this.overDraft+" "+super.toString();
+    }
+
+    @Override
+    public String getType() {
+        return "CurrentAccount";
+    }
 }
--- src/main/java/net/zakaria/model/SavingAccount.java
+++ src/main/java/net/zakaria/model/SavingAccount.java
@@ -1,2 +1,34 @@
-package net.zakaria.model;public class SavingAccount {
+package net.zakaria.model;
+
+public final class SavingAccount extends BankAccount{
+    private double interestRate;
+    public SavingAccount(){
+        super();
+    }
+    public SavingAccount(double balance, String currency, double interestRate){
+        super(balance, currency);
+        this.interestRate = interestRate;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public String toString() {
+        return "SavingAccount { interestRate = "+ this.interestRate+" "+super.toString();
+    }
+
+    @Override
+    public String getType() {
+        return "Saving_Account";
+    }
+    /*@Override
+    public void print(){
+
+    }*/
 }
